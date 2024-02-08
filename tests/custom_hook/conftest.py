14beb1073e5b57e53ff834597532d0ca63a1ce8e
ericsalesdeandrade <sdaeric19@gmail.com>
2024-02-08 20:35:42 +0000

    Remove custom hook

--- tests/custom_hook/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.hookimpl()
-def pytest_my_hook(config):
-    print("Hello from `pytest_my_hook` hook!")
-    print(f"Config: {config}")
--- tests/custom_hook/test_custom_hook.py
+++
@@ -1,3 +0,0 @@
-def test_custom_hook_pass():
-    print("Running Test Custom Hook")
-    assert True
